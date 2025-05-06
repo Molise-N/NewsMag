64fcb840403f4a5e5958f3addc4c17b0c407047e
Molise Ntlhoki <158150348+Molise-N@users.noreply.github.com>
2025-05-06 18:02:17 +0200

    Update NewsItem.jsx

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,15 +1,33 @@
 import image from '../assets/news.jpg';
-const NewsItem = ({title,description,src,url}) => {
-  return (
-    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
-        <img src={src? src: image} style={{height: "200px",width:"325px"}}className="card-img-top" alt="..."/>
-        <div className="card-body">
-            <h5 className="card-title">{title.slice(0,50)}</h5>
-            <p className="card-text"> {description? description.slice(0,90):"News    current event. It is information about something that just happened"}</p>
-            <a href={url} className="btn btn-primary">Read More</a>
-        </div>
-    </div>
-  )
-}
 
-export default NewsItem
+const NewsItem = ({ title, description, src, url }) => {
+  // Fallbacks
+  const safeTitle = title ? title.slice(0, 50) : "Untitled News";
+  const safeDescription = description
+    ? description.slice(0, 90)
+    : "Current event. Information about something that just happened.";
+  const safeUrl = url || "#";
+
+  return (
+    <div
+      className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2"
+      style={{ maxWidth: "345px" }}
+    >
+      <img
+        src={src || image}
+        style={{ height: "200px", width: "325px" }}
+        className="card-img-top"
+        alt="News"
+      />
+      <div className="card-body">
+        <h5 className="card-title">{safeTitle}</h5>
+        <p className="card-text">{safeDescription}</p>
+        <a href={safeUrl} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
